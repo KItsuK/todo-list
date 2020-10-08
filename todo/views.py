ad5d8fd5507124830892d53f7f821f17c4dca222
Kanan <kanan.31352762684@icloud.com>
2020-10-08 10:30:56 +0900

    作成、更新、詳細、削除urlと汎用ビューの作成

--- todo/views.py
+++ todo/views.py
@@ -7,3 +7,18 @@ from .models import Todo
 class IndexView(generic.ListView):
     model = Todo
     paginate_by = 5
+
+class DetailView(generic.DetailView):
+    model = Todo
+
+class CreateView(generic.CreateView):
+    model = Todo
+    success_url = reverse_lazy('index')
+
+class UpdateView(generic.UpdateView):
+    model = Todo
+    success_url =reverse_lazy('index')
+
+class DeleteView(generic.DeleteView):
+    model = Todo
+    success_url = reverse_lazy('index')
