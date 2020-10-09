e2f0e2a9d35bab13412d224dd2c210629f934b6c
Kanan <kanan.31352762684@icloud.com>
2020-10-09 15:00:51 +0900

    テンプレートの適用とフォームの改善

--- todo/urls.py
+++ todo/urls.py
@@ -6,7 +6,7 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('crate/', views.CreateView.as_view(), name='create'),
+    path('create/', views.CreateView.as_view(), name='create'),
     path('<int:pk>/update', views.UpdateView.as_view(), name='update'),
     path('<int:pk>/delete', views.DeleteView.as_view(), name='delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import TodoForm
 
 from .models import Todo
 
@@ -14,10 +15,12 @@ class DetailView(generic.DetailView):
 class CreateView(generic.CreateView):
     model = Todo
     success_url = reverse_lazy('index')
+    form_class = TodoForm
 
 class UpdateView(generic.UpdateView):
     model = Todo
     success_url =reverse_lazy('index')
+    form_class = TodoForm
 
 class DeleteView(generic.DeleteView):
     model = Todo
